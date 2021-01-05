559c440c6cdc937b079ced2ce821cfe29c51e1d5
manassogale132 <73747525+manassogale132@users.noreply.github.com>
2021-01-05 12:03:22 +0530

    Added : Ability to add hotel names and rates for regular customers.

--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -15,31 +15,7 @@ public class HotelReservation {
         functions.addHotel();
         functions.showHotelInfo();
 
-        System.out.println(" ");
-        System.out.println("*Cheapest Hotel Based on WeekdayRegularRate");
-        functions.findCheapestHotelOne("01-Jan-2021","05-Jan-2021");
-        System.out.println(" ");
-
-        System.out.println(" ");
-        System.out.println("*Cheapest Hotel Based on WeekendRegularRate");
-        functions.findCheapestHotelTwo("01-Jan-2021","05-Jan-2021");
-        System.out.println(" ");
-
-        System.out.println(" ");
-        System.out.println("*Cheapest best Rated Hotel Based on WeekendRegularRate");
-        functions.findCheapestHotelRaitingForRegular("01-Jan-2021","05-Jan-2021");
-        System.out.println(" ");
-
-        System.out.println(" ");
-        System.out.println("*Cheapest best Rated Hotel Based on WeekendRewardRate");
-        functions.findCheapestHotelRaitingForReward("01-Jan-2021","05-Jan-2021");
-        System.out.println(" ");
-
-
-        System.out.println(" ");
-        System.out.println("*Cheapest best Rated Hotel Based on WeekdayRewardRate");
-        functions.findCheapestHotelRaitingForRewardTwo("01-Jan-2021","05-Jan-2021");
-        System.out.println(" ");
+      
         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
     }
 
