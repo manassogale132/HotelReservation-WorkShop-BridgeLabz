2c26b6dc9560c50f89f76d8fe93fd0b53dc14f9a
manassogale132 <73747525+manassogale132@users.noreply.github.com>
2021-01-05 12:07:13 +0530

    Added : Ability to find cheapest hotel for given rangedate.

--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -28,6 +28,25 @@ public class Functions {
             System.out.println(myHotelList.get(i));
         }
     }
+    
+ 
+    public void findCheapestHotelOne(String startDateRange, String endDateRange) {                                                      //Find CheapestHotel based on weekdayRegularRate
+
+        LocalDate startDate = LocalDate.parse(startDateRange, DateTimeFormatter.ofPattern("d-MMM-yyyy"));        //LocalDate class is used to parse the start and end date ranges with DateTimeFormater class to pass date format
+        LocalDate endDate = LocalDate.parse(endDateRange, DateTimeFormatter.ofPattern("d-MMM-yyyy"));
+
+        int numberOfDays = endDate.getDayOfMonth() - startDate.getDayOfMonth() + 1;                                 //To calculate Number of days stayed + the weekdayRegularRate for eg-(5*110)
+
+        Optional<Hotel> cheapestHotel = this.myHotelList.stream().sorted(Comparator.comparing(Hotel::getWeekdayRegularRate)).findFirst();  //Stream function on arrlits ie-myHotelList and storing in cheapestHotel
+
+        Hotel hotel = new Hotel();                                                                                  //Hotel Object
+        hotel.setHotelName(cheapestHotel.get().getHotelName());
+        hotel.setTotal(cheapestHotel.get().getWeekdayRegularRate() * numberOfDays);                            //numberOfDays = for eg-(5*110)
+
+        System.out.println("-HotelName :" + hotel.getHotelName());                                        //Printing Hotel name and total Hotel rate for the date range
+        System.out.println("-NumberOfDaysStayed * WeekdayRegularRate :" + hotel.getTotal() + "$");
+
+    }
 
 
 }
