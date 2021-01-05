82448823891fc91053eb797e16695d44b07b06c7
manassogale132 <73747525+manassogale132@users.noreply.github.com>
2021-01-05 12:21:07 +0530

    Added : Ability to find cheapest best rated hotel for given daterange for Reward customer rates.

--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -26,6 +26,22 @@ public class HotelReservation {
         System.out.println("*Cheapest Hotel Based on WeekendRegularRate");
         functions.findCheapestHotelTwo("01-Jan-2021","05-Jan-2021");
         System.out.println(" ");
+        
+        System.out.println(" ");
+        System.out.println("*Cheapest best Rated Hotel Based on WeekendRegularRate");
+        functions.findCheapestHotelRaitingForRegular("01-Jan-2021","05-Jan-2021");
+        System.out.println(" ");
+        
+        System.out.println(" ");
+        System.out.println("*Cheapest best Rated Hotel Based on WeekendRewardRate");
+        functions.findCheapestHotelRaitingForReward("01-Jan-2021","05-Jan-2021");
+        System.out.println(" ");
+
+
+        System.out.println(" ");
+        System.out.println("*Cheapest best Rated Hotel Based on WeekdayRewardRate");
+        functions.findCheapestHotelRaitingForRewardTwo("01-Jan-2021","05-Jan-2021");
+        System.out.println(" ");
 
       
         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
